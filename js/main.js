0003ba0762a841dd078865727f409e18f2e6eae0
MacAhmad <118671209+MacAhmad@users.noreply.github.com>
2022-11-20 15:59:49 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,98 @@
+/*Navbar*/ 
+$(function(){
+    $(".sidenav").sidenav();
+})
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+});
+
+/*see more*/
+$(function(){
+    $("#pics").hide();
+
+    let pict = $("#pics");
+    let btn1 = $("#bt1");
+        btn1.on("click",function(){
+            pict.slideToggle("fast");
+        })
+})
+
+$(function(){
+    $("#pics2").hide();
+
+    let pict2 = $("#pics2");
+    let btn2 = $("#bt2");
+        btn2.on("click",function(){
+            pict2.slideToggle("fast");
+        })
+})
+
+$(function(){
+    $("#pics3").hide();
+
+    let pict3 = $("#pics3");
+    let btn3 = $("#bt3");
+        btn3.on("click",function(){
+            pict3.slideToggle("fast");
+        })
+})
+
+$(function(){
+    $("#img1").hide();
+
+    let image1 = $("#img1");
+    let btt1 = $("#bt01");
+        btt1.on("click",function(){
+            image1.slideToggle("fast");
+        })
+})
+
+$(function(){
+    $("#img2").hide();
+
+    let image2 = $("#img2");
+    let btt2 = $("#bt02");
+        btt2.on("click",function(){
+            image2.slideToggle("fast");
+        })
+})
+
+$(function(){
+    $("#img3").hide();
+
+    let image3 = $("#img3");
+    let btt3 = $("#bt03");
+        btt3.on("click",function(){
+            image3.slideToggle("fast");
+        })
+})
+
+$(function(){
+    $("#img4").hide();
+
+    let image4 = $("#img4");
+    let btt4 = $("#bt04");
+        btt4.on("click",function(){
+            image4.slideToggle("fast");
+        })
+})
+
+$(function(){
+    $("#img5").hide();
+
+    let image5 = $("#img5");
+    let btt5 = $("#bt05");
+        btt5.on("click",function(){
+            image5.slideToggle("fast");
+        })
+})
+
+$(function(){
+    $("#img6").hide();
+
+    let image6 = $("#img6");
+    let btt6 = $("#bt06");
+        btt6.on("click",function(){
+            image6.slideToggle("fast");
+        })
+})
